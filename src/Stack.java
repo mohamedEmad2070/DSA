777c820fad2fe715448ca8c11d50849a31bb5453
Mohamed Emad <mohpopqu@gmail.com>
2024-02-13 07:57:42 +0200

    stack

---
+++ src/Stack.java
@@ -0,0 +1,65 @@
+public class Stack {
+    int top;
+    int maxSize = 100;
+    int item[] = new int[maxSize];
+
+    public Stack() {
+        top = -1;
+    }
+
+    boolean isEmpty() {
+        return top == -1;
+    }
+
+    void push(int val) {
+        if (top > maxSize) {
+            System.out.println("Stack Is Full");
+        } else {
+            top++;
+            item[top] = val;
+        }
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Is Empty");
+        } else {
+            top--;
+        }
+    }
+    int getPop() {
+        int val;
+        if (isEmpty()) {
+            System.out.println("Stack Is Empty");
+            return 0;
+        } else {
+            val = item[top];
+            top--;
+            return val;
+        }
+    }
+
+    int getTop() {
+        int val;
+        if (isEmpty()) {
+            System.out.println("Stack Is Empty");
+            return 0;
+        } else {
+            val = item[top];
+            return val;
+        }
+    }
+
+    void print(){
+        System.out.print("[ ");
+        for (int i = top; i >= 0 ; i--) {
+            System.out.print(item[i]+" ");
+
+        }
+        System.out.print("]");
+        System.out.println();
+
+    }
+
+
+}
