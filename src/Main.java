0d53950773412ee922e76226cfcbc11d764b4dc8
Mohamed Emad <158189000+mohamedEmad2070@users.noreply.github.com>
2024-02-18 19:38:41 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-    Stack s =new Stack();
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    s.print();
-    System.out.println(s.getPop());
-    s.print();
     }
 }
